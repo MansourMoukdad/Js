5edeb67eac168747c385ab4fe81607e79c7c1abb
MansourMoukdad <moukdad1@gmail.com>
2025-04-18 00:13:17 +0300

    Data Type

--- DataTypes-And-Operators/DataType-Operators.js
+++ DataTypes-And-Operators/DataType-Operators.js
@@ -1,4 +1,17 @@
+/* Type of Data
+    - Strign
+    - Number
+    - Array => Object
+    - Object 
+    - Boolean
+*/
 console.log("Mansour");
 console.log(typeof("Mansour"));
 console.log(typeof(20));
 console.log(typeof(["Mansour", 37, 1988]));
+console.log(typeof({name: "Mansour", age: 37, country: "Syria"}));
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null);
+
